33f0f39ac000587490900c7d15ac4d7c128bbc26
AndrIvasc <andrejus.iv1880@gmail.com>
2025-01-27 16:12:39 +0200

    Pertvakyta templates

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ def home():
     search_text = request.args.get("searchlaukelis")
     if search_text:
         filtered_row = Projektas.query.filter(Projektas.pavadinimas.ilike(f"{search_text}"))
-        return render_template("index_css.html", projects=filtered_row)
+        return render_template("index.html", projects=filtered_row)
     else:
         # all_projects = session.query(Projektas).all() <- atititkmuo is praeitos tiemos sqlalchemy
         all_projects = Projektas.query.all()  # <- flask-sqlalchemy
-        return render_template("index_css.html", projects=all_projects)
+        return render_template("index.html", projects=all_projects)
 
 
 @app.route("/projektas/<int:row_id>")
